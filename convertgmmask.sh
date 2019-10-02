a70451df7d4328e007e156641300a40283121ff0
Soichi Hayashi <hayashis@iu.edu>
2019-10-02 14:46:43 +0000

    fixed typo

--- convertgmmask.sh
+++ convertgmmask.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-
+echo "converting aparc+aseg.mgz to gm_mask.nii.gz"
 fsurfer=`jq -r '.freesurfer' config.json`
-
-#I don't think we need to do this 
-#source $FREESURFER_HOME/SetUpFreeSurfer.sh
-
-mkdir -p freesurfer
-
 mri_binarize --i $fsurfer/mri/aparc+aseg.mgz --gm --o gm_mask.mgz
 mri_convert gm_mask.mgz gm_mask.nii.gz
 
