c4e89b7ce600366fa9ab892df46acdeec6050fbd
Soichi Hayashi <hayashis@iu.edu>
2019-10-02 14:43:29 +0000

    putting gmmask bask as aparc+aseg is actually not used, and instead gmmask is used somewhree

--- convertaseg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-fsurfer=`jq -r '.freesurfer' config.json`
-mri_convert $fsurfer/mri/aparc+aseg.mgz aparc+aseg.nii.gz
-mri_convert $fsurfer/mri/aparc.a2009s+aseg.mgz aparc.a2009s+aseg.nii.gz
--- convertgmmask.sh
+++ convertgmmask.sh
@@ -6,12 +6,8 @@ fsurfer=`jq -r '.freesurfer' config.json`
 #I don't think we need to do this 
 #source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
-#mkdir -p freesurfer/mri
-
-mkdir -p freesurfer/mri
+mkdir -p freesurfer
 
 mri_binarize --i $fsurfer/mri/aparc+aseg.mgz --gm --o gm_mask.mgz
-mri_convert gm_mask.mgz freesurfer/gm_mask.nii.gz
-
-
+mri_convert gm_mask.mgz gm_mask.nii.gz
 
