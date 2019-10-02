04054d7e35bf2b5bd7f3304a9c0bcf6ea1add083
Soichi Hayashi <hayashis@iu.edu>
2019-10-02 13:31:53 +0000

    updated how aparc+aseg.nii.gz is generated

--- convertgmmask.sh
+++ convertgmmask.sh
@@ -3,12 +3,15 @@
 
 fsurfer=`jq -r '.freesurfer' config.json`
 
-source $FREESURFER_HOME/SetUpFreeSurfer.sh
+#I don't think we need to do this 
+#source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+#mkdir -p freesurfer/mri
 
 mkdir -p freesurfer/mri
 
-mri_binarize --i $fsurfer/mri/aparc+aseg.mgz --gm --o  $fsurfer/gm_mask.mgz
+mri_binarize --i $fsurfer/mri/aparc+aseg.mgz --gm --o gm_mask.mgz
+mri_convert gm_mask.mgz freesurfer/gm_mask.nii.gz
 
-mri_convert $fsurfer/gm_mask.mgz  freesurfer/gm_mask.nii.gz
 
 
